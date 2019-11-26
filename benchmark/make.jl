8af8f4566af9aedf751c80168535397b4f23cac7
Maxime Mouchet <max@maxmouchet.com>
2019-11-26 14:48:47 +0100

    benchmark: add todo

--- benchmark/make.jl
+++ benchmark/make.jl
@@ -2,6 +2,7 @@ using PkgBenchmark
 using HMMBase
 
 # TODO: Add timestamp to result files
+# TODO: Compare with latest tag
 # results = benchmarkpkg(parent(pathof(HMMBase)))
 results = benchmarkpkg(pwd(), resultfile = "benchmark.json")
 export_markdown("benchmark.md", results)
